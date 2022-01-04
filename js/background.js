8cece3681971f204a27382cf37549bd591c1e875
kaleidoscopess <kaleidoscopes233@gmail.com>
2022-01-04 15:54:55 +0800

    增加WebVPN情况支持

--- js/background.js
+++ js/background.js
@@ -2,6 +2,6 @@ chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     return { redirectUrl: chrome.extension.getURL("js/core_fixA1.js") };
   },
-  { urls: ["http://111.116.20.13/js/core.js", "http://*.ccut.cn/js/core.js", "http://*.ccut.edu.cn/js/core.js"] },
+  { urls: ["http://111.116.20.13/js/core.js", "http://*.ccut.cn/js/core.js", "http://*.ccut.edu.cn/js/core.js", "https://111-116-20-13.webvpn.ccut.cn/js/core.js"] },
   ["blocking"]
 );
